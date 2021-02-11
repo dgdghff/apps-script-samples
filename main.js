39b724ef91b2d93f281bd07fca5413e1823b7471
C.Hernandez <76412594+dgdghff@users.noreply.github.com>
2021-02-10 18:09:01 -0700

    add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+// String, Numbers, Boolean, null, undefined
+
+   const name = 'Maria';
+   const age = 48;
+   const email = cassandrahernandez959@gmail.com;
+   const phone number = 480-526-6272;
+   const address = 340 n 28 th dr 1 phx az 85009
+   const updated = true;
+   const web browser = chrome;
+   const ssid = CenturyLink5350;
+   const security type = WPA2-AES;
+   const gui = 192.168.0.1;
+   const home = 602-269-2350;
+   const defaultdomain = dgdghff.github.io;
+   const business email = dgdghff.github.io@centurylink.net;
+   const recipients = fioa504@fioa.gov, egille@warrenton.va.gov;
+   const fax = (540)349-2414;
+   const phone = (540) 347-1101 x 209;
+   const sanbox.paypal.com = sb-391jo805451@personal.example.com;
+   const Domain/Username = cassandrahernandez959@gmail.com;
+   const API = BChxzjViV5GzkRhOgRddImj9VDK7uZoSiBAtOu8;
+   const Account_ID = 0c9a5e00-c682-402d-a2cd-de670b4f9811;
+   const 
+   const x = null;
+   const y = undefined;
+   let z;
+
+   // Concatentation
+   console.log('My name is ' + name + ' I am ' + age ' + ' to recieve email ' + email + ' Myrouter ' + ssid + + ' modem gui ' + gui + ' type ' + security ' + sending mail ' + recipients + ' sender ' + Domain + ' email_in_transport ' + API);
+					console.log(String);
+   // Template String
+   const console.log = `My name is ${name} i am ${age} to recieve mail ${email} router ${ssid} modem gui ${gui} type ${security}  sending mail to ${recipients} sender ${Domain} in transport ${API}`;
+					console.log(main.js)
+
+   // Arrays - variables that hold multiple values
+
+   const numbers = new Array(1,2,3,4,5,6,7,8,9,0);
+
+   console.log(numbers);
+
+   /* multi
+	line
+	comments */
+				
+   // Arrays
+
+   const login_ids = ['cassandrahernande959@gmail.com', 'dgdghff', 'dgdghff.github.io@centurylink.net', 'marshallty', 'sb-391jo805451@personal.example.com', 'sb-dqwvf806406', 'bebe18', 'maria@isabelh.network', 'isabelh33', 'kevingates', 'cassandrahernandez959'];
+
+   console.log(login_ids);
+
+   /*to get indexof
+	console.log(login_ids.indexOf('maria@isabelh.network'));
+	
+	const FIOA Officer = {
+	   firstName: 'Elizabeth',
+		lastName: 'Gillie',
+		email: 'egillie@warrenton.va.gov',
+		fax: '(540) 349-2414',
+		phone: '(540) 347-1101 x 209',
+		}
+	}
+	
+	console.log(FIOA officer.name);<iframe src="demo_iframe_sandbox(no_values).html sandbox!></iframe>
+"
